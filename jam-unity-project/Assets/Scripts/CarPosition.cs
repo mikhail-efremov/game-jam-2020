9f3ca6630072abfbe95e7e42587c5193b406e332
alexanderkarpetc <alexander.karpetc@gmail.com>
2020-10-23 21:36:23 +0300

    Add base timelapse logic

---
+++ jam-unity-project/Assets/Scripts/CarPosition.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+  public struct CarPosition
+  {
+    public readonly Vector3 position;
+    public readonly Quaternion rotation;
+
+    public CarPosition(Vector3 position, Quaternion rotation)
+    {
+      this.position = position;
+      this.rotation = rotation;
+    }
+  }
+}
