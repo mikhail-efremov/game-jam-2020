1417e4cc3136c0db53aec4b0fcf68e8c97ae4d8e
Mikhail Efremov <mikhail.a.efremov@gmail.com>
2020-10-24 11:56:42 +0300

    Link smoke to cars

--- jam-unity-project/Assets/Scripts/Tire.cs
+++ jam-unity-project/Assets/Scripts/Tire.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 
-public class Tire : MonoBehaviour {
+public class Tire : MonoBehaviour
+{
+	[SerializeField] private ParticleSystem _smoke;
 
 	public float RestingWeight { get; set; }
 	public float ActiveWeight { get; set; }
@@ -15,6 +17,12 @@ public class Tire : MonoBehaviour {
 	bool TrailActive;
 	GameObject Skidmark;
 
+	private void Start()
+	{
+		if (_smoke != null)
+			_smoke.Stop();
+	}
+
 	public void SetTrailActive(bool active) {
 		if (active && !TrailActive) {
 			// These should be pooled and re-used
@@ -25,8 +33,14 @@ public class Tire : MonoBehaviour {
 			
 			Skidmark.GetComponent<TrailRenderer> ().sortingOrder = 0;
 			Skidmark.transform.parent = transform;
+			
+			if (_smoke != null)
+				_smoke.Play();
 		} else if (!active && TrailActive) {			
 			Skidmark.transform.parent = null;
+			
+			if (_smoke != null)
+				_smoke.Stop();
 		}
 		TrailActive = active;
 	}
