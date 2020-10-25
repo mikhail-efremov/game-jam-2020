d9c72103f52269d594fe0fdfe036dcbc73c6bd63
alexanderkarpetc <alexander.karpetc@gmail.com>
2020-10-25 12:36:03 +0200

    Add water hydrant

---
+++ jam-unity-project/Assets/Scripts/WaterHydrant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterHydrant : MonoBehaviour
+{
+    [SerializeField] private GameObject _waterFx;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Instantiate(_waterFx, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
